b7bf32844a7aba13ad8fabf8e235468423609186
deivyrossi <deivyrossi@gmail.com>
2025-04-02 09:08:14 -0300

    Implementação da leitura de matriz

---
+++ src/arquivo.hpp
@@ -0,0 +1,12 @@
+#ifndef ARQUIVO_HPP
+#define ARQUIVO_HPP
+
+#include <string>
+using namespace std;
+
+class Arquivo {
+public:
+    void processarArquivo(const string& nomeArquivo);
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "arquivo.hpp"
+
+int main() {
+    Arquivo arq;
+    arq.processarArquivo("input.dat");
+    return 0;
+}
