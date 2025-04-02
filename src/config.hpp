e1e0c4d25f3debb9bd28ccd3da5f0c8ee048794f
deivyrossi <deivyrossi@gmail.com>
2025-04-02 14:45:12 -0300

    Primeira implementacao dos algoritmos, nao rodam ainda

---
+++ src/config.hpp
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define VENTO_CIMA 1
+#define VENTO_BAIXO 0
+#define VENTO_ESQUERDA 0
+#define VENTO_DIREITA 1
+
+#define MAX_ITERACOES 100
+
+#define TERRENO_LIVRE 0
+#define ARVORE_SAUDAVEL 1
+#define ARVORE_EM_CHAMAS 2
+#define ARVORE_QUEIMADA 3
+#define AGUA 4
+
+#endif 
