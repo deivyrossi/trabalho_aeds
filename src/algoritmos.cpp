de1229c441ba5867153b6191eb49fe32a73770ab
deivyrossi <deivyrossi@gmail.com>
2025-04-02 23:21:32 -0300

    Mais alguma funcoes e implementacoes - ainda nao roda

--- src/algoritmos.cpp
+++ src/algoritmos.cpp
@@ -11,6 +11,11 @@ using namespace std;
 #include "arquivo.hpp"
 #include "config.hpp"
 
+struct  Posicao {
+    int x;
+    int y;
+};
+
 
 void PegarFogo(int linhas, int colunas, vector<vector<int>>& matriz){
     vector<vector<int>> novaMatriz = matriz;
@@ -19,7 +24,7 @@ void PegarFogo(int linhas, int colunas, vector<vector<int>>& matriz){
     int xAnimal, yAnimal = 0;
     encontrarCaminho(xAnimal, yAnimal);
 
-    while(iterator != matriz.end()){
+    while(iterator i != matriz.end()){
         if(matriz[i][j] !== 2){
             return -1;
         }
@@ -65,6 +70,40 @@ void PegarFogo(int linhas, int colunas, vector<vector<int>>& matriz){
         }
     }
 
+    matriz = novaMatriz;
+
+    dx = {0, 0, -1, 1};
+    dy = {1, -1, 0, 0};
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            nx = i + dx[k];
+            ny = j + dy[k];
+            if(nx > 0 && nx < linhas && ny > 0 && ny < colunas && matriz[nx][ny] == 1){
+                novaMatriz[nx][ny] = 2;
+
+            }
+        }
+    }
+
+    if(matriz[xAnimal][yAnimal] == 2 || matriz[xAnimal][yAnimal] == 3){
+        cout << "O animal morreu";
+        return -1;
+        
+    }
+
+    if(matriz[xAnimal][yAnimal] == 4){
+        matriz[xAnimal][yAnimal] = 0;
+        for(int i = 0; i < linhas; i++){
+            for(int j = 0; j < colunas; j++){
+                if (matriz[i+1][j] && matriz[i-1][j] && matriz[i][j+1] && matriz[i][j-1] == 2){
+                    matriz[i][j] = 1;
+                }
+            }
+        }
+    }
+
+
+
 
 
 
@@ -72,8 +111,45 @@ void PegarFogo(int linhas, int colunas, vector<vector<int>>& matriz){
 
 }
 
+void encontrarMelhorPos(vector<vector<int>>& matriz, int linhas, int colunas){
+    vector<Posicao> direcoes = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    Posicao melhorpos = {0, 0};
+    int melhorValor = 3
+
+    for(const auto& direcao : direcoes){
+        int nx = x + direcao.x;
+        int ny = y + direcao.y;
+
+        if(nx >= 0 && nx < linhas && ny >= 0 && ny < colunas){
+            int valor = matriz[nx][ny];
+
+            if(valor == 4){
+            return {nx, ny};} else if(valor == 0 || valor == 1 && melhorValor == 3){
+                imelhorPos = {nx, ny};
+                melhorValor = valor;
+            }
+        
+        }
+    }
+
+    return melhorPos;
+
+
+
+}
+
 void inicializarMatriz(vector<vector<int>>& matriz, int linhas, int colunas, int valorInicial) {
     matriz.resize(linhas, vector<int>(colunas, valorInicial));
+}
+
+
+void imprimirMatriz(const vector<vector<int>>& matriz) {
+    for (const auto& linha : matriz) {
+        for (const auto& elemento : linha) {
+            cout << elemento << " ";
+        }
+        cout << endl;
+    }
 }
 
 
@@ -123,7 +199,7 @@ void encontrarMelhorOp(){
     }
 }
 
-void 
+
 
 
 
