f52573eb098372c9f4ae2f4fa880e6492bf3ba78
deivyrossi <deivyrossi@gmail.com>
2025-04-03 11:41:51 -0300

    compilou a primeira vez

---
+++ src/algoritmos.hpp
@@ -0,0 +1,28 @@
+#ifndef ALGORITMOS_HPP
+#define ALGORITMOS_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <list>
+#include <stdexcept>
+#include <sstream>
+
+struct Posicao {
+    int x;
+    int y;
+};
+
+
+
+
+class Algoritmos {
+public:
+    void PegarFogo(int linhas, int colunas, vector<vector<int>>& matriz);
+    Posicao encontrarMelhorPos(vector<vector<int>>& matriz, int linhas, int colunas, int x, int y);
+    Posicao encontrarCaminho(const vector<vector<int>>& matriz, int linhas, int colunas);
+    void imprimirMatriz(const vector<vector<int>>& matriz);
+};
+
+#endif
